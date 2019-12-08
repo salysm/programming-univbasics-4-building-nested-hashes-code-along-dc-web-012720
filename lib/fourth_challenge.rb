07aec3263dad3f528cb7c63478652cba5ebcf3c6
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 07:42:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,6 +8,11 @@ def fourth_challenge
       :name => "Benvolio",
       :age => "17",
       :attitude => "worried"
+      } {
+      :name => "Mercutio",
+      :age =>  "18",
+      :attitude => "hot-headed"
+      }
       ]
    }, 
    :capulet => {
